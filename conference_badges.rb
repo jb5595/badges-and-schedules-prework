99f794967cc6459a8d2bb428587e69cde2c5d03b
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 20:40:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,12 +3,12 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator(names){
+def batch_badge_creator(names)
   badges = Array.new
   names.each {|name|
   badges.push(badge_maker(name))
   }
-}
+end
 
 # def assign_rooms(speakers){
 #   assignments = Array.new
